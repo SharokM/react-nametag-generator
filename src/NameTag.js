047df8bc32112eb1037c083bff07a2fdc57969a9
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-03-15 14:49:16 +0000

    initial commit

---
+++ src/NameTag.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NameTag = (props) => (
+  <div className="nameTag">
+    <h3 className="title">Hello</h3>
+    <p className="subtitle">My name is</p>
+    <h2 className="name">{props.name}</h2>
+  </div>
+);
+
+export default NameTag;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NameTag from "./NameTag";
+import "./styles.css";
+
+const App = () => (
+  <div className="App">
+    <h1>Name Tag Generator</h1>
+    <NameTag name = {"James"} />
+    <NameTag name = {"Samar"} />
+    <NameTag name = {"Becky"} />
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
